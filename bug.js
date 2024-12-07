036acee2ef2ee582a9d91f938073e2a480847099
BugHunter-X <askzenai@gmail.com>
2024-12-07 10:46:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Unhandled exception in the server
+//process.on('uncaughtException', function (err) {
+//  console.error('Unhandled Exception:', err);
+//});
